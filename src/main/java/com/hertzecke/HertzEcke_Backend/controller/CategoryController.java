ad7e24668c705aa4e53f9eba8c74c5fc7449a45b
Amirreza Naderi <amirrezanaderi@Amirrezas-MacBook-Pro.local>
2026-08-16 17:50:03 +0330

    adding category controller

---
+++ src/main/java/com/hertzecke/HertzEcke_Backend/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.hertzecke.HertzEcke_Backend.controller;
+
+
+import com.hertzecke.HertzEcke_Backend.dto.CategoryDTO;
+import com.hertzecke.HertzEcke_Backend.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+}
---
+++ src/main/java/com/hertzecke/HertzEcke_Backend/service/CategoryService.java
@@ -0,0 +1,24 @@
+package com.hertzecke.HertzEcke_Backend.service;
+
+
+import com.hertzecke.HertzEcke_Backend.dto.CategoryDTO;
+import com.hertzecke.HertzEcke_Backend.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryDTO> getAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(CategoryDTO::categoryEntityToDTO)
+                .toList();
+    }
+}
